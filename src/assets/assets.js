7768f34aed0158f801fb53904f8b2643e787e75f
Rajibbepary <rajib635356@gmail.com>
2025-10-24 08:16:12 +0600

    Added all image assets and configured assets.js for export

---
+++ src/assets/assets.js
@@ -0,0 +1,94 @@
+
+import arrow_icon_dark from './arrow-icon-dark.png'
+import arrow_icon from './arrow-icon.png'
+import circular_text from './circular-text.png'
+import close_black from './close-black.png'
+import close_white from './close-white.png'
+import code_icon_dark from './code-icon-dark.png'
+import code_icon from './code-icon.png'
+import dev_icon from './dev-icon.png'
+import download from './download-icon.png'
+import edu_icon from './edu-icon-dark.png'
+import figma from './figma.png'
+import firebase from './firebase.png'
+import foooter_bg from './footer-bg-color.png'
+import git from './git.png'
+import graphics_icon from './graphics-icon.png'
+import hand_icon from './hand-icon.png'
+import header_bg_color from './header-bg-color.png'
+import logo from './logo.png'
+import logo_dark from './logo_dark.png'
+import mail_icon from './mail_icon.png'
+import menu_black from './menu-black.png'
+import menu_white from './menu-white.png'
+import mobile from './mobile-icon.png'
+import mongodb from './mongodb.png'
+import moon_icon from './moon_icon.png'
+import profile_img from './profile-img (2).png'
+import project_icon_dark from './project-icon-dark.png'
+import project_icon from './project-icon.png'
+import react from './react.svg'
+import right_arrow_bold_dark from './right-arrow-bold-dark.png'
+import right_arrow_bold from './right-arrow-bold.png'
+import right_arrow_dark from './right-arrow-dark.png'
+import right_arrow_white from './right-arrow-white.png'
+import right_arrow from './right-arrow.png'
+import send_icon from './send-icon.png'
+import sun_icon from './sun_icon.png'
+import ui_icon from './ui-icon.png'
+import user_imag from './user-image.png'
+import user_profile from './user-profile.png'
+import vscode from './vscode.png'
+import web_icon from './web-icon.png'
+import work_1 from './work-1.png'
+import work_2 from './work-2.png'
+import work_3 from './work-3.png'
+import work_4 from './work-4.png'
+const assets = {
+    arrow_icon_dark,
+    arrow_icon,
+    circular_text,
+    close_black,
+    close_white,
+    code_icon_dark,
+    code_icon,
+    dev_icon,
+    download,
+    edu_icon,
+    figma,
+    firebase,
+    foooter_bg,
+    git,
+    graphics_icon,
+    hand_icon,
+    header_bg_color,
+    logo,
+    logo_dark,
+    mail_icon,
+    menu_black,
+    menu_white,
+    mobile,
+    mongodb,
+    moon_icon,
+    profile_img,
+    project_icon_dark,
+    project_icon,
+    react,
+    right_arrow_bold_dark,
+    right_arrow_bold,
+    right_arrow_dark,
+    right_arrow_white,
+    right_arrow,
+    send_icon,
+    sun_icon,
+    ui_icon,
+    user_imag,
+    user_profile,
+    vscode,
+    web_icon,
+    work_1,
+    work_2,
+    work_3,
+    work_4
+}
+export default assets;
